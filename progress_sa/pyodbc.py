d727ecb8775cf7dde712476c8f9d36ee4bd488bb
isma-data <dominik.pegler@inject-star.at>
2022-04-26 23:25:55 +0200

    driver name

--- progress_sa/pyodbc.py
+++ progress_sa/pyodbc.py
@@ -1,13 +1,14 @@
 from progress_sa.base import ProgressDialect, ProgressExecutionContext
 from sqlalchemy.connectors.pyodbc import PyODBCConnector
 from sqlalchemy.util import asbool
+import sys
+
 
 class ProgressExecutionContext_pyodbc(ProgressExecutionContext):
     pass
 
 class Progress_pyodbc(PyODBCConnector, ProgressDialect):
-    # For OpenEdge 10. Progress 9 uses Progress_SQL92_Driver.
-    pyodbc_driver_name = 'Progress_SQL_Driver'
+    pyodbc_driver_name = "DataDirect 7.1 Progress OpenEdge Wire Protocol" #"Progress OpenEdge 11.7 Driver"
     execution_ctx_cls = ProgressExecutionContext_pyodbc
 
     def __init__(self, description_encoding='latin-1', **kwargs):
